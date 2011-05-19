bd922175769b0bec122542b48a11293ced7b9950
y.sravankumar <y.sravankumar@gmail.com>
2011-05-19 02:00:52 +0000

    Initial import.

---
+++ OpenGL ES2/es2particles/src/es2/learning/ParticleManager.java
@@ -0,0 +1,5 @@
+package es2.learning;
+
+public class ParticleManager {
+
+}
---
+++ OpenGL ES2/es2particles/src/es2/learning/ParticleView.java
@@ -0,0 +1,13 @@
+package es2.learning;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class ParticleView extends GLSurfaceView {
+
+	public ParticleView(Context context) {
+		super(context);
+		
+	}
+
+}
