0cdf29f6f61d6226df0b0b8be98c4ae167815a02
y.sravankumar@gmail.com <y.sravankumar@gmail.com@102950c4-5ec2-885e-6f9b-b25a7ce37e60>
2011-05-19 14:18:26 +0000

    got textured particles... need to arrange them and move them

--- trunk/OpenGL ES2/es2particles/src/es2/learning/ParticleView.java
+++ trunk/OpenGL ES2/es2particles/src/es2/learning/ParticleView.java
@@ -5,9 +5,12 @@ import android.opengl.GLSurfaceView;
 
 public class ParticleView extends GLSurfaceView {
 
+	public ParticleManager mgr;
 	public ParticleView(Context context) {
 		super(context);
 		
+		mgr = new ParticleManager(this);
+		mgr.Setup(0, 0, 0);
 	}
 
 }
