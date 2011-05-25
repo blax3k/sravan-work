c51569ef8c1ae8cfb00d006ec84aa3db7cf24d23
y.sravankumar@gmail.com <y.sravankumar@gmail.com@102950c4-5ec2-885e-6f9b-b25a7ce37e60>
2011-05-25 14:38:23 +0000

    particle system without using ES2/Shaders

---
+++ trunk/OpenGL ES2/particle_test/src/test/particle/particletest.java
@@ -0,0 +1,19 @@
+package test.particle;
+
+import test.particle.ParticleView;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class particletest extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        ParticleView pv = new ParticleView(this);
+        setContentView(pv);
+    }
+}
