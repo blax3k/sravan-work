b7c86f51038f03545d62350b32baf152f5aa3ddc
y.sravankumar@gmail.com <y.sravankumar@gmail.com@102950c4-5ec2-885e-6f9b-b25a7ce37e60>
2011-05-29 11:14:15 +0000

    just added renderer

--- trunk/OpenGL ES2/es2viewport/src/es2/learning/es2ViewPort.java
+++ trunk/OpenGL ES2/es2viewport/src/es2/learning/es2ViewPort.java
@@ -1,13 +1,23 @@
 package es2.learning;
 
 import android.app.Activity;
+import android.opengl.GLSurfaceView;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class es2ViewPort extends Activity {
+	GLSurfaceView  view;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        view = new GLSurfaceView(this);
+        view.setEGLContextClientVersion(2);
+        view.setRenderer(new ViewPortRenderer());
+        setContentView(view);
     }
 }
