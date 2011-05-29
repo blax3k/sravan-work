6325f535a5a34b56028eabf22c1ca16167985c6a
y.sravankumar@gmail.com <y.sravankumar@gmail.com@102950c4-5ec2-885e-6f9b-b25a7ce37e60>
2011-05-29 09:47:05 +0000

    setting the view port

---
+++ trunk/OpenGL ES2/es2viewport/src/es2/learning/es2ViewPort.java
@@ -0,0 +1,13 @@
+package es2.learning;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class es2ViewPort extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
