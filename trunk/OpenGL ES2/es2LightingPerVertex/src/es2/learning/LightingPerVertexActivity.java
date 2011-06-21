8299aeae4480615399e38a94f79c82f240391da0
y.sravankumar@gmail.com <y.sravankumar@gmail.com@102950c4-5ec2-885e-6f9b-b25a7ce37e60>
2011-06-21 02:52:31 +0000

    Initial import.

---
+++ trunk/OpenGL ES2/es2LightingPerVertex/src/es2/learning/LightingPerVertexActivity.java
@@ -0,0 +1,21 @@
+package es2.learning;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class LightingPerVertexActivity extends Activity {
+	ES2SurfaceView  view;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        
+        view = new ES2SurfaceView(this);
+        
+        setContentView(view);
+    }
+}
---
+++ trunk/OpenGL ES2/es2LightingPerVertex/src/es2/learning/PerVertexRenderer.java
@@ -0,0 +1,33 @@
+package es2.learning;
+
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
+import android.opengl.GLSurfaceView.Renderer;
+
+public class PerVertexRenderer implements Renderer {
+	public float xAngle = 0;
+	public float yAngle = 0;
+	public PerVertexRenderer(ES2SurfaceView es2SurfaceView) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void onDrawFrame(GL10 arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSurfaceChanged(GL10 gl, int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
