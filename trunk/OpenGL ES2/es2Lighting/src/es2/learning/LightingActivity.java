f179475c0dd561c1b3068d2667f35362aa6dd0a8
y.sravankumar@gmail.com <y.sravankumar@gmail.com@102950c4-5ec2-885e-6f9b-b25a7ce37e60>
2011-06-25 07:39:04 +0000

    added menu option to switch between per-vertex and per-pixel lighting

--- trunk/OpenGL ES2/es2Lighting/src/es2/learning/LightingActivity.java
+++ trunk/OpenGL ES2/es2Lighting/src/es2/learning/LightingActivity.java
@@ -2,6 +2,8 @@ package es2.learning;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.Window;
 import android.view.WindowManager;
 
@@ -17,5 +19,27 @@ public class LightingActivity extends Activity {
         view = new ES2SurfaceView(this);
         
         setContentView(view);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	menu.add(Menu.NONE, 0, Menu.NONE, "Per Vertex");
+    	menu.add(Menu.NONE, 1, Menu.NONE, "Per Pixel");
+      return super.onCreateOptionsMenu(menu);
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	
+    	if (item.getItemId() == 0)
+    	{
+    		view.LoadProgram(item.getItemId());
+    	} else if (item.getItemId() == 1)
+    	{
+    		view.LoadProgram(item.getItemId());
+    	} else {	
+    		return super.onOptionsItemSelected(item);
+    	}
+    	return true;
     }
 }
