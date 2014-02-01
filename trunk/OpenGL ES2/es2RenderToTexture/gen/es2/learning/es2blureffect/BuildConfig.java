ff983b2066d2733b9bb351877111662259a1a583
y.sravankumar@gmail.com <y.sravankumar@gmail.com@102950c4-5ec2-885e-6f9b-b25a7ce37e60>
2014-02-01 13:31:25 +0000

    initial import

---
+++ trunk/OpenGL ES2/es2RenderToTexture/gen/es2/learning/es2blureffect/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package es2.learning.es2blureffect;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ trunk/OpenGL ES2/es2RenderToTexture/src/es2/learning/es2rendertotexture/RTTSurfaceView.java
@@ -0,0 +1,60 @@
+package es2.learning.es2rendertotexture;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+
+public class RTTSurfaceView extends GLSurfaceView {
+
+	RTTRenderer renderer;
+	float touchedX = 0;
+	float touchedY = 0;
+	
+	public RTTSurfaceView(Context context) {
+		super(context);
+		setEGLContextClientVersion(2);
+		renderer = new RTTRenderer(this);
+		setRenderer(renderer);
+	}
+
+	public RTTSurfaceView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event)
+	{
+		if (event.getAction() == MotionEvent.ACTION_DOWN)
+		{
+			touchedX = event.getX();
+			touchedY = event.getY();
+		} else if (event.getAction() == MotionEvent.ACTION_MOVE)
+		{
+			renderer.xAngle += (touchedX - event.getX())/2f;
+			renderer.yAngle += (touchedY - event.getY())/2f;
+			
+			/*if(renderer.yAngle > 70)
+				renderer.yAngle = 70;
+			
+			if(renderer.xAngle > 70)
+				renderer.xAngle = 70;
+			
+			if(renderer.xAngle < -70)
+				renderer.xAngle = -70;
+			
+			if(renderer.yAngle < -70)
+				renderer.yAngle = -70;
+			
+			Log.d("RTT", "renderer.xAngle:"+renderer.xAngle+" renderer.yAngle:"+renderer.xAngle);*/
+			
+			touchedX = event.getX();
+			touchedY = event.getY();
+		}
+		return true;
+		
+	}
+
+}
